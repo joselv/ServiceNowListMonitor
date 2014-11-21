892278eebebb70f965e1c84a56be198c6a0bccd6
Jose Valle <jose.valle@servicenow.com>
2014-11-21 08:29:50 -0800

    use options

--- background.js
+++ background.js
@@ -1,20 +1,36 @@
+var rateOfRefresh = 60;
+
 function refreshCount() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://hi.service-now.com/incident.do?JSONv2&sysparm_action=getRecords&sysparm_query=active=true^assigned_to=javascript:getMyAssignments()^u_action_needed=true", true);
-	xhr.onreadystatechange = function() {
-  		if (xhr.readyState == 4) {
-    		// JSON.parse does not evaluate the attacker's scripts.
-    		var resp = JSON.parse(xhr.responseText);
-    		chrome.browserAction.setBadgeText({text: resp.records.length.toString()});
-    		var activeNumbers = [];
-    		for (var i = resp.records.length - 1; i >= 0; i--) {
-    			activeNumbers.push(resp.records[i].number);
-    		};
-    		chrome.storage.local.set({'values':activeNumbers});
-    	    chrome.storage.local.getBytesInUse(null,function(v){console.log(v);});
-  		}
-	}
-	xhr.send();
+
+        // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        query: 'incident.do?JSONv2&sysparm_action=getRecords&sysparm_query=active=true^assigned_to=javascript:getMyAssignments()^u_action_needed=true',
+        rate: 10,
+        values: []
+    }, function(items) {
+        rateOfRefresh = items.rate;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://hi.service-now.com/" + items.query, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                // JSON.parse does not evaluate the attacker's scripts.
+                var resp = JSON.parse(xhr.responseText);
+                chrome.browserAction.setBadgeText({
+                    text: resp.records.length.toString()
+                });
+                var activeNumbers = [];
+                for (var i = resp.records.length - 1; i >= 0; i--) {
+                    activeNumbers.push(resp.records[i].number);
+                };
+                chrome.storage.sync.set({
+                    'values': activeNumbers
+                });
+            }
+        }
+        xhr.send();
+    });
 }
+
+
 refreshCount();
-var intervalID = window.setInterval(refreshCount, 60000);
+var intervalID = window.setInterval(refreshCount, rateOfRefresh * 1000);
---
+++ options.js
@@ -0,0 +1,35 @@
+function save_options() {
+    //get values from elements on form
+    var encodedQ = document.getElementById('encodedQuery');
+    var refreshR = document.getElementById('refreshRate');
+
+    //save values to local storage
+    chrome.storage.sync.set({
+        query: encodedQ.value,
+        rate: refreshR.value
+    }, function() {
+        // Update status to show that options were saved 
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() { status.textContent = '';}, 750);
+    });
+}
+
+// stored in chrome.storage.
+function restore_options() {
+    console.log('starting loading options');
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        query: 'incident.do?JSONv2&sysparm_action=getRecords&sysparm_query=active=true^assigned_to=javascript:getMyAssignments()^u_action_needed=true',
+        rate: 60
+    }, function(items) {
+        console.log('in call back of restore_options');
+        console.log(items);
+        document.getElementById('encodedQuery').value = items.query;
+        document.getElementById('refreshRate').value = items.rate;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+
+document.getElementById('save').addEventListener('click', save_options);
