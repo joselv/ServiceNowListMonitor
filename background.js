c20251f90aaa2e0c271c463330707286cf5110cd
Jose Valle <jose.valle@servicenow.com>
2014-11-22 12:45:49 -0800

    added ability to disable notifications

--- background.js
+++ background.js
@@ -7,7 +7,8 @@ function refreshCount() {
     chrome.storage.sync.get({
         query: 'incident.do?JSONv2&sysparm_action=getRecords&sysparm_query=active=true^assigned_to=javascript:getMyAssignments()^stateIN-40,2^ORu_action_needed=true^u_action_needed=true^ORstate=-40',
         rate: 10,
-        values: []
+        values: [],
+        nofications: false
     }, function(items) {
 
         rateOfRefresh = items.rate;
@@ -36,35 +37,38 @@ function refreshCount() {
                 chrome.storage.sync.set({
                     'values': newIncidentList
                 });
-                var newlyAdded = getNewIncidents(currentIncidentList, newIncidentList);
-                var newlyUpdated = getRecenlyUpdated(currentIncidentList, newIncidentList);
-                if (newlyAdded.length > 0) {
-                    chrome.notifications.create('newIncident', {
-                        iconUrl: "icon.png",
-                        type: 'list',
-                        title: 'New Incidents',
-                        message: 'Incidents recently added to list',
-                        priority: 1,
-                        items: newlyAdded
-                    }, function(notificationId) {
-                        console.log("Last error:", chrome.runtime.lastError)
-                    })
+                if (items.nofications) {
+                    var newlyAdded = getNewIncidents(currentIncidentList, newIncidentList);
+                    var newlyUpdated = getRecenlyUpdated(currentIncidentList, newIncidentList);
+                    if (newlyAdded.length > 0) {
+                        chrome.notifications.create('newIncident', {
+                            iconUrl: "icon.png",
+                            type: 'list',
+                            title: 'New Incidents',
+                            message: 'Incidents recently added to list',
+                            priority: 1,
+                            items: newlyAdded
+                        }, function(notificationId) {
+                            console.log("Last error:", chrome.runtime.lastError)
+                        })
+                    }
+                    if (newlyUpdated.length > 0) {
+                        chrome.notifications.create('UpdatedIncident', {
+                            iconUrl: "icon.png",
+                            type: 'list',
+                            title: 'Updated Incidents',
+                            message: 'Incidents in list recently Updated',
+                            priority: 1,
+                            items: newlyUpdated
+                        }, function(notificationId) {
+                            if (chrome.runtime.lastError) {
+                                console.log("Last error:", chrome.runtime.lastError);
+                            }
+                        })
+                    }
+
+
                 }
-                if (newlyUpdated.length > 0) {
-                    chrome.notifications.create('UpdatedIncident', {
-                        iconUrl: "icon.png",
-                        type: 'list',
-                        title: 'Updated Incidents',
-                        message: 'Incidents in list recently Updated',
-                        priority: 1,
-                        items: newlyUpdated
-                    }, function(notificationId) {
-                        console.log("Last error:", chrome.runtime.lastError)
-                    })
-                }
-
-
-
             }
         }
 
--- options.js
+++ options.js
@@ -2,11 +2,12 @@ function save_options() {
     //get values from elements on form
     var encodedQ = document.getElementById('encodedQuery');
     var refreshR = document.getElementById('refreshRate');
-
+    var notiEnabled = document.getElementById('enableNotifications');
     //save values to local storage
     chrome.storage.sync.set({
         query: encodedQ.value,
-        rate: refreshR.value
+        rate: refreshR.value,
+        nofications:notiEnabled.checked
     }, function() {
         // Update status to show that options were saved 
         var status = document.getElementById('status');
@@ -21,12 +22,14 @@ function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
         query: 'incident.do?JSONv2&sysparm_action=getRecords&sysparm_query=active=true^assigned_to=javascript:getMyAssignments()^u_action_needed=true',
-        rate: 10
+        rate: 10,
+        nofications:true
     }, function(items) {
         console.log('in call back of restore_options');
         console.log(items);
         document.getElementById('encodedQuery').value = items.query;
         document.getElementById('refreshRate').value = items.rate;
+        document.getElementById('enableNotifications').checked = items.nofications ? true : false;
     });
 }
 
