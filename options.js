b3836f44172f7c733f7f0f130f20808b6bbed43b
Jose Valle <jose@josevalle.net>
2015-01-31 11:22:15 -0800

    added warning for refresh rate below 60 seconds and error for non numeric refresh rate

--- options.js
+++ options.js
@@ -1,33 +1,25 @@
 function save_options() {
-    //get values from elements on form
-    var encodedQ = document.getElementById('encodedQuery');
-    var refreshR = document.getElementById('refreshRate');
-    var notiEnabled = document.getElementById('enableNotifications');
     //save values to local storage
+    //TODO:add some validation
+    var rr = document.getElementById('refreshRate').value;
     chrome.storage.sync.set({
-        query: encodedQ.value,
-        rate: refreshR.value,
-        nofications:notiEnabled.checked
+        query: document.getElementById('encodedQuery').value,
+        rate: isThisNumeric(rr) ? rr : 120,
+        nofications: document.getElementById('enableNotifications').checked
     }, function() {
-        // Update status to show that options were saved 
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() { status.textContent = '';}, 750);
+        //TODO:need a way to give feedback to user that changes were saved
     });
 }
 
 // stored in chrome.storage.
 function restore_options() {
-    console.log('starting loading options');
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
         query: 'active=true^assigned_to=javascript:getMyAssignments()^u_action_needed=true',
         rate: 10,
-        nofications:true,
-        avgTime:[]
+        nofications: true,
+        avgTime: []
     }, function(items) {
-        console.log('in call back of restore_options');
-        console.log(items);
         var sum = _.reduce(items.avgTime, function(memo, num){ return memo + num; }, 0);
         document.getElementById('encodedQuery').value = items.query;
         document.getElementById('refreshRate').value = items.rate;
@@ -36,23 +28,21 @@ function restore_options() {
     });
 }
 
-function isThisNumeric(val)  {
-    return !isNaN(parseFloat(val)) && isFinite(val);
-}
+function isThisNumeric(val) {return (/^[0-9]+$/.test(val));}
+
+function hideIt(element) { document.getElementById(element).classList.add('hideIt'); }
+
+function showIt(element) { document.getElementById(element).classList.remove('hideIt'); }
+
 
 document.addEventListener('DOMContentLoaded', restore_options);
 
 document.getElementById('save').addEventListener('click', save_options);
 
-document.getElementById("refreshRate").addEventListener('change',function(){      console.log('change noticed');
-    if (isThisNumeric(this.value)){
-      console.log('value is numeric');
-      if (parseInt(this.value)<60) {
-        console.log('value is less than 60');
-        var msg = document.getElementById("errorMsg");
-        //show warning about value lower than 60.
-      } 
-    } else {
-      //show error about numeric value
-    }
-},true);
+document.getElementById("refreshRate").addEventListener('change', function() {
+    hideIt("warningMsg");
+    hideIt("errorMsg");
+    if (isThisNumeric(this.value)) {
+        if (parseInt(this.value, 10) < 60) { showIt("warningMsg");}
+    } else { showIt('errorMsg'); }
+}, true);
